697200af7dafb8bca313a9c6533a30a665914bcc
Aditya Dahake <dahakeadi15@gmail.com>
2025-11-30 23:00:27 +0530

    model and initialization

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"Buy Apples", "Buy Dates", "Buy IceCream"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 }
-
