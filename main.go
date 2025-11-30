e29e967e7a18df2c06453b096e6a253db2f232b5
Aditya Dahake <dahakeadi15@gmail.com>
2025-11-30 23:15:06 +0530

    update method

--- main.go
+++ main.go
@@ -23,6 +23,31 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			m.cursor--
+
+		case "down", "j":
+			m.cursor++
+
+		case "enter", "space":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 }
