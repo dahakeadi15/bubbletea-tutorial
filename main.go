bd98e1d9ee91c49eea3f6bc2365c4a6067172547
Aditya Dahake <dahakeadi15@gmail.com>
2025-11-30 23:29:48 +0530

    add logging to file

--- main.go
+++ main.go
@@ -72,6 +72,15 @@ func (m model) View() string {
 }
 
 func main() {
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
