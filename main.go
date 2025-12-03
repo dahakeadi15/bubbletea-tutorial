12a0b4d3ec4791458fb6d2251c3b33d635adbbb3
Aditya Dahake <dahakeadi15@gmail.com>
2025-12-03 12:27:37 +0530

    update method

--- main.go
+++ main.go
@@ -14,13 +14,12 @@ type model struct {
 	err    error
 }
 
-type statusMsg struct {
-	status int
-}
-
-type errMsg struct {
-	err error
-}
+type (
+	statusMsg int
+	errMsg    struct {
+		err error
+	}
+)
 
 func checkServer() tea.Msg {
 	c := &http.Client{Timeout: time.Second * 10}
@@ -28,7 +27,7 @@ func checkServer() tea.Msg {
 	if err != nil {
 		return errMsg{err}
 	}
-	return statusMsg{res.StatusCode}
+	return statusMsg(res.StatusCode)
 }
 
 func (e errMsg) Error() string { return e.err.Error() }
@@ -38,3 +37,23 @@ func (e errMsg) Error() string { return e.err.Error() }
 func (m model) Init() tea.Cmd {
 	return checkServer
 }
+
+// Internally `Cmd`s run in a goroutine, and the `Msg` they return is sent
+// to the Update function for handling
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case statusMsg:
+		m.status = int(msg)
+		return m, tea.Quit
+	case errMsg:
+		m.err = msg
+		return m, tea.Quit
+	case tea.KeyMsg:
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string { return "" }
