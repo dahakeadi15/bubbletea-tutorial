06b86353696a6389fca3f32eb77a51e209fbb4d6
Aditya Dahake <dahakeadi15@gmail.com>
2025-12-03 12:13:31 +0530

    all commands and messages

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
+import (
+	"net/http"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
 const url = "https://charm.sh/"
 
 type model struct {
 	status int
 	err    error
 }
+
+type statusMsg struct {
+	status int
+}
+
+type errMsg struct {
+	err error
+}
+
+func checkServer() tea.Msg {
+	c := &http.Client{Timeout: time.Second * 10}
+	res, err := c.Get(url)
+	if err != nil {
+		return errMsg{err}
+	}
+	return statusMsg{res.StatusCode}
+}
+
+func (e errMsg) Error() string { return e.err.Error() }
